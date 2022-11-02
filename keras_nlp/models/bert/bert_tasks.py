a24c9ba2da51c20896202228809924d322c72fd7
Matt Watson <mattdangerw@gmail.com>
2022-11-02 16:49:15 -0700

    Port the BertClassifier to be a pipeline

--- keras_nlp/models/bert/bert_tasks.py
+++ keras_nlp/models/bert/bert_tasks.py
@@ -13,23 +13,30 @@
 # limitations under the License.
 """BERT task specific models and heads."""
 
+import copy
+
 from tensorflow import keras
 
 from keras_nlp.models.bert.bert_models import Bert
 from keras_nlp.models.bert.bert_models import bert_kernel_initializer
+from keras_nlp.models.bert.bert_preprocessing import BertPreprocessor
+from keras_nlp.models.bert.bert_presets import backbone_presets
+from keras_nlp.models.utils import classproperty
+from keras_nlp.utils.pipeline_model import PipelineModel
 
 # TODO(jbischof): Find more scalable way to list checkpoints.
 CLASSIFIER_DOCSTRING = """BERT encoder model with a classification head.
 
     Args:
-        backbone: A string or `keras_nlp.models.Bert` instance. If a string,
-            should be one of {names}.
+        backbone: A string or `keras_nlp.models.Bert` instance.
+        preprocessor: A `keras_nlp.models.BertPreprocessor`. If `None`, inputs
+            will not be preprocessed automatically.
         num_classes: int. Number of classes to predict.
 
     Examples:
     ```python
-    # Randomly initialized BERT encoder
-    model = keras_nlp.models.BertCustom(
+    # Randomly initialized BERT backbone.
+    model = keras_nlp.models.Bert(
         vocabulary_size=30522,
         num_layers=12,
         num_heads=12,
@@ -37,78 +44,78 @@ CLASSIFIER_DOCSTRING = """BERT encoder model with a classification head.
         intermediate_dim=3072,
         max_sequence_length=12
     )
+    # Preset preprocessing.
+    preprocessor = keras_nlp.models.BertPreprocessor("bert_base_uncased_en")
 
     # Call classifier on the inputs.
-    input_data = {{
-        "token_ids": tf.random.uniform(
-            shape=(1, 12), dtype=tf.int64, maxval=model.vocabulary_size
-        ),
-        "segment_ids": tf.constant(
-            [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0], shape=(1, 12)
-        ),
-        "padding_mask": tf.constant(
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0], shape=(1, 12)
-        ),
-    }}
-    classifier = keras_nlp.models.BertClassifier(model, 4, name="classifier")
-    logits = classifier(input_data)
-
-    # String backbone specification
+    input_data = tf.constant(["Classify this sentence!"])
     classifier = keras_nlp.models.BertClassifier(
-        "bert_base_uncased_en", 4, name="classifier"
-    )
-    logits = classifier(input_data)
+        backbone=model,
+        preprocessor=preprocessor,
+        num_classes=2)
+    logits = classifier.predict(input_data)
 
-    # Access backbone programatically (e.g., to change `trainable`)
+    # Use a preset backbone and preprocessing
+    classifier = keras_nlp.models.BertClassifier(
+        "bert_base_uncased_en",
+        num_classes=2,
+    )
+    logits = classifier.predict(input_data)
+
+    # Access backbone programatically (e.g., to change `trainable`).
     classifier.backbone.trainable = False
+
+    # Access preprocessor programatically (e.g., to run preprocessing).
+    classifier.preprocessor("Preprocess this sentence!")
     ```
 """
 
 
 @keras.utils.register_keras_serializable(package="keras_nlp")
-class BertClassifier(keras.Model):
+class BertClassifier(PipelineModel):
     def __init__(
         self,
-        backbone="bert_base_uncased_en",
+        backbone,
+        preprocessor=None,
         num_classes=2,
         **kwargs,
     ):
-        # Load backbone from string identifier
-        # TODO(jbischof): create util function when ready to load backbones in
-        # other task classes (e.g., load_backbone_from_string())
-        if isinstance(backbone, str):
-            if backbone not in Bert.presets:
-                raise ValueError(
-                    "`backbone` must be one of "
-                    f"""{", ".join(Bert.presets)}. Received: {backbone}."""
-                )
-            backbone = Bert.from_preset(backbone)
-
         inputs = backbone.input
         pooled = backbone(inputs)["pooled_output"]
         outputs = keras.layers.Dense(
             num_classes,
             kernel_initializer=bert_kernel_initializer(),
-            name="logits",
+            name="preds",
         )(pooled)
         # Instantiate using Functional API Model constructor
         super().__init__(
             inputs=inputs,
             outputs=outputs,
+            include_preprocessing=preprocessor is not None,
             **kwargs,
         )
         # All references to `self` below this line
         self._backbone = backbone
+        self._preprocessor = preprocessor
         self.num_classes = num_classes
+
+    def preprocess_samples(self, x, y=None, sample_weight=None):
+        return self.preprocessor(x, y=y, sample_weight=sample_weight)
 
     @property
     def backbone(self):
-        """A `keras_nlp.models.Bert` instance providing the encoder submodel."""
+        """A `keras_nlp.models.Bert` instance."""
         return self._backbone
+
+    @property
+    def preprocessor(self):
+        """A `keras_nlp.models.BertPreprocessor` for preprocessing."""
+        return self._preprocessor
 
     def get_config(self):
         return {
             "backbone": keras.layers.serialize(self.backbone),
+            "preprocessor": keras.layers.serialize(self.preprocessor),
             "num_classes": self.num_classes,
             "name": self.name,
             "trainable": self.trainable,
@@ -118,11 +125,31 @@ class BertClassifier(keras.Model):
     def from_config(cls, config):
         if "backbone" in config:
             config["backbone"] = keras.layers.deserialize(config["backbone"])
+        if "preprocessor" in config:
+            config["preprocessor"] = keras.layers.deserialize(
+                config["preprocessor"]
+            )
         return cls(**config)
+
+    @classproperty
+    def presets(cls):
+        return copy.deepcopy(backbone_presets)
+
+    @classmethod
+    def from_preset(
+        cls,
+        preset,
+        **kwargs,
+    ):
+        if "backbone" not in kwargs:
+            kwargs["backbone"] = Bert.from_preset(preset)
+        if "preprocessor" not in kwargs:
+            kwargs["preprocessor"] = BertPreprocessor.from_preset(preset)
+        return cls(**kwargs)
 
 
 setattr(
     BertClassifier,
     "__doc__",
-    CLASSIFIER_DOCSTRING.format(names=", ".join(Bert.presets)),
+    CLASSIFIER_DOCSTRING,
 )
